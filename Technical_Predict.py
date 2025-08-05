11c2144feffc9ece87d43c202a3bc271c033b7dd
Wangsa9 <cnugrah2003@yahoo.com>
2025-08-05 15:02:00 +0800

    CCS Prediction.py

--- Technical_Predict.py
+++ Technical_Predict.py
@@ -13,7 +13,7 @@ import io
 from matplotlib.ticker import FuncFormatter
 
 st.set_page_config(
-    page_title="CCS Prediction 2025",
+    page_title="Carbon Capture Storage Prediction 2025",
     page_icon="⭐",
     initial_sidebar_state="expanded"
 )
